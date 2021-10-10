b9763c6600341214152493e0efde360a86b2fede
freeedie666 <freeedie666@gmail.com>
2021-10-10 22:12:47 +0200

    deleted bitch

--- models/models.py
+++ models/models.py
@@ -1,17 +1,7 @@
 from shared_db import db
 #TODO doplnit nullable
 
-class Bitch(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(80))
-    email = db.Column(db.String(120))
 
-    def __init__(self, username, email):
-        self.username = username
-        self.email = email
-
-    def __repr__(self):
-        return '<User %r>' % self.username
 
 
 class User(db.Model):
